9688dc232c7573f7622499131d6859b711fd6503
bbartok <32492812+bbartok@users.noreply.github.com>
2017-11-03 19:17:28 -0600

    test file for inserting values into database
    as of first upload, only has values for agent and customer

---
+++ test-insert.sql
@@ -0,0 +1,6 @@
+insert into agents values ('a20', 'bill', '123');
+insert into agents values ('a30', 'tom', '456');
+insert into agents values ('a40', 'edd', '789');
+
+insert into customers values('c20', 'john', '123 45ave', '123');
+insert into customers values('c30', 'joe', '6703 127st', '456');
